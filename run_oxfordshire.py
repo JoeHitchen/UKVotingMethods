308b0804990420fb61b78d8b7ac0bf3c3c46f5bc
Joe Hitchen <hitchen@atm.ox.ac.uk>
2017-06-09 23:29:57 +0100

    Added simple Oxfordshire election

---
+++ run_oxfordshire.py
@@ -0,0 +1,65 @@
+import json
+import re
+
+from wiki_scraper import get_constituency_results
+from voting_engines import DirectElectionEngine
+
+
+# Load settings and data
+with open('./data/parties.json') as file:
+    parties = json.load(file)
+with open('./data/oxfordshire.json') as file:
+    groups = json.load(file)
+with open('./data/results_2015.json') as file:
+    results = json.load(file)
+
+
+# Loop over voting groups
+for key in groups:
+    group = groups[key]
+    
+    # Link parties
+    for const in group['Constituencies']:
+        for candidate in results[const]:
+            for party in parties:
+                if candidate['party'] in parties[party]['aliases']:
+                    candidate['party'] = party
+
+
+## Prepare and run election
+# Loop over voting groups
+for key in groups:
+    group = groups[key]
+    
+    # Get candidates
+    candidates = []
+    for const in group['Constituencies']:
+        candidates.extend(results[const])
+        
+    # Get redistribution matrix
+    matrix = {}
+    for from_cand in candidates:
+        
+        # Check for party redistribution
+        from_party = from_cand['party']
+        if not from_party in parties or 'redistribute' not in parties[from_party]:
+            continue
+        
+        # Create candidate redistribution
+        matrix[from_cand['name']] = {}
+        for to_cand in candidates:
+            if to_cand['party'] in parties[from_party]['redistribute']:
+                matrix[from_cand['name']][to_cand['name']] = parties[from_party]['redistribute'][to_cand['party']]
+    
+    
+    # Run election
+    election = DirectElectionEngine(
+        [candidate['name'] for candidate in candidates],
+        seats = 6,
+        votes = {candidate['name']: candidate['votes'] for candidate in candidates},
+        redistribution_matrix = matrix
+    )
+    election.run_election()
+    print(election.quota)
+    print(election.elected)
+    
